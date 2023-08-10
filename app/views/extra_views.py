282b9ee1387133522a950604b68271912263f34e
SantiagoAHS <lionelsanti2018@gmail.com>
2023-08-10 09:52:31 -0600

    05 version 1.5.2

--- app/views/extra_views.py
+++ app/views/extra_views.py
@@ -10,4 +10,16 @@ def terminos():
 def politicas():
     return render_template('extras/Politicas.html')
 
+@extra_views.route("/ayudaadmin")
+def ayudaadmin():
+    return render_template('extras/ayuda.html')
+
+@extra_views.route("/ayudageneral")
+def ayudageneral():
+    return render_template('extras/ayuda1.html')
+
+@extra_views.route("/ayuda")
+def ayuda():
+    return render_template('extras/ayuda2.html')
+
 
